d69bc058ca60186f3bc0829dec8f89664d907dc5
kawnayeen <kawnayeen.08@gmail.com>
2019-07-09 22:54:17 +0600

    layout of header section imported

--- src/component/App.js
+++ src/component/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import avatar from '../asset/img/avatar.jpg';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faAd} from '@fortawesome/free-solid-svg-icons'
+import {faHome, faGraduationCap, faFolderOpen, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import {faHackerrank, faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 const App = () => {
@@ -18,10 +18,10 @@ const App = () => {
                             <div className="p-5 bg-inverse text-white">
                                 <div className="name d-flex flex-row justify-content-between align-items-center">
                                     <h1 className="display-4">John Doe</h1>
-                                    <div><FontAwesomeIcon icon={faHackerrank} size="2x"/></div>
-                                    <div><FontAwesomeIcon icon={faGithub} size="2x"/></div>
-                                    <div><FontAwesomeIcon icon={faLinkedin} size="2x"/></div>
-                                    <div><FontAwesomeIcon icon={faTwitter} size="2x"/></div>
+                                    <div><FontAwesomeIcon icon={faHackerrank} size="lg"/></div>
+                                    <div><FontAwesomeIcon icon={faGithub} size="lg"/></div>
+                                    <div><FontAwesomeIcon icon={faLinkedin} size="lg"/></div>
+                                    <div><FontAwesomeIcon icon={faTwitter} size="lg"/></div>
                                 </div>
                             </div>
                             <div className="p-4 bg-dark">Experienced Full Stack Web Developer</div>
@@ -29,19 +29,30 @@ const App = () => {
                                 <div className="d-flex flex-row text-white align-items-stretch text-center">
                                     <div className="port-item p-4 bg-primary" data-toggle="collapse"
                                          data-target="#home">
-                                        <i className="fa fa-home d-block"></i> Home
+                                        <span className="d-block">
+                                        <FontAwesomeIcon icon={faHome} size="lg"/>
+                                        </span>
+                                        Home
                                     </div>
                                     <div className="port-item p-4 bg-success" data-toggle="collapse"
                                          data-target="#resume">
-                                        <i className="fa fa-graduation-cap d-block"></i> Resume
+                                        <span className="d-block">
+                                        <FontAwesomeIcon icon={faGraduationCap} size="lg"/>
+                                        </span>
+                                        Resume
                                     </div>
                                     <div className="port-item p-4 bg-warning" data-toggle="collapse"
                                          data-target="#work">
-                                        <i className="fa fa-folder-open d-block"></i> Work
+                                        <span className="d-block">
+                                        <FontAwesomeIcon icon={faFolderOpen} size="lg"/>
+                                        </span>
+                                        Work
                                     </div>
                                     <div className="port-item p-4 bg-danger" data-toggle="collapse"
                                          data-target="#contact">
-                                        <i className="fa fa-envelope d-block"></i> Contact
+                                        <span className="d-block">
+                                        <FontAwesomeIcon icon={faEnvelope} size="lg"/>
+                                        </span> Contact
                                     </div>
                                 </div>
                             </div>
