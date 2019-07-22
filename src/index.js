e6702f84f3c0331c140a3ffdc3ae400dde855c60
kawnayeen <kawnayeen.08@gmail.com>
2019-07-22 22:13:46 +0600

    redux integrated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './component/App';
 import './asset/css/bootstrap.css';
 import './asset/css/style.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './reducer';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
