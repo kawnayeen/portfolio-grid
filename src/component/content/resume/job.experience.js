f5b399da20cb356daed7389189c73af64f06f990
kawnayeen <kawnayeen.08@gmail.com>
2019-08-16 20:32:09 +0600

    job experience redesigned

--- src/component/content/resume/job.experience.js
+++ src/component/content/resume/job.experience.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 const JobExperience = ({experience}) => {
+    const responsibilities = experience.getJobDescription().map(val => <li>{val}</li>);
+    const ulStyle = {
+        padding: '0px 0px 0px 18px',
+        fontSize: '13px'
+    };
     return (
         <div className="card mb-3">
             <div className="card-block">
                 <h4 className="card-title">{experience.getCompanyName()}</h4>
-                <p className="card-text">{experience.getJobDescription()}</p>
                 <p className="p-2 mb-3 bg-inverse text-white">
                     {experience.getPosition()}
                 </p>
                 <p className="p-2 mb-3 bg-inverse text-white">
                     {experience.getCompanyPhone()}
                 </p>
+                <p className="card-text"><ul style={ulStyle}>{responsibilities}</ul></p>
             </div>
             <div className="card-footer">
                 <h6 className="text-muted">{experience.getDuration()}</h6>
--- src/reducer/experience/experience.reducer.js
+++ src/reducer/experience/experience.reducer.js
@@ -4,7 +4,12 @@ export default () => [
     new Experience(
         'Cefalo AS',
         '+88 02 9131954',
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, neque!',
+        [
+            'Working as full stack engineer, from db design to customer facing gui',
+            'Developing frontend application using React with Redux',
+            'Developing RESTful services using Ktor framework',
+            'Conducting verbal technical interview'
+        ],
         'Senior Software Engineer',
         2019,
         'present'
@@ -12,7 +17,11 @@ export default () => [
     new Experience(
         'Cefalo AS',
         '+88 02 9131954',
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, neque!',
+        [
+            'Android & iOS application development using native technologies',
+            'Build automation for Android and iOS project',
+            'Architecting fault tolerant cloud infrastructure for micro-services developed in Spring technology'
+        ],
         'Software Engineer',
         2017,
         2019
@@ -20,7 +29,12 @@ export default () => [
     new Experience(
         'Pechas Game Studios',
         '+88 02 9893151',
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, neque!',
+        [
+            'Overseer the development & deployment of multiple game product for Android & iOS platform.',
+            'Designing business objects of the system and defining the interfaces of different modules',
+            'Developing web backend to provide all the RESTful services required for the game',
+            'Breaking down the development process in different milestones & achieve them'
+        ],
         'Lead Developer',
         2014,
         2017
